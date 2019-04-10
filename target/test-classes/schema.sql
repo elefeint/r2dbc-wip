3ca1ee5d845b070262e4ca825bcd2008f95b0d4e
Elena Felder <41136058+elefeint@users.noreply.github.com>
2019-04-10 12:16:02 -0400

    removing transient files

--- target/test-classes/schema.sql
+++
@@ -1,7 +0,0 @@
-create table book (
-  id int,
-  title varchar(64)
-);
-
-insert into book (id, title) values (1, 'harry potter');
-insert into book (id, title) values (2, 'on the origin of species');
